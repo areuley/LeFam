62683ffbddb5c5afe921d035eca9dff63e8717c3
areuley <72866649+areuley@users.noreply.github.com>
2026-02-10 20:26:56 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,6 @@
+var GAS_API_URL = 'https://script.google.com/macros/s/AKfycbzm1heZA6dcYx27REb0xN9_UoP7NPGjE9RnW-c2BdXh8schWMhiRqQiym852rEHJAel/exec';
+var PUSH_USER = 'Anthony';
+
 self.addEventListener('install', function(event) {
   self.skipWaiting();
 });
@@ -31,22 +34,39 @@ self.addEventListener('push', function(event) {
   );
 });
 
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  var page = (event.notification.data && event.notification.data.page) || 'dashboard';
-  
+self.addEventListener('push', function(event) {
   event.waitUntil(
-    clients.matchAll({ type: 'window' }).then(function(clientList) {
-      for (var i = 0; i < clientList.length; i++) {
-        if (clientList[i].focus) {
-          clientList[i].focus();
-          clientList[i].postMessage({ action: 'navigate', page: page });
-          return;
-        }
+    fetch(GAS_API_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'text/plain' },
+      body: JSON.stringify({ action: 'getLatestUnread', args: [PUSH_USER] })
+    })
+    .then(function(response) { return response.json(); })
+    .then(function(result) {
+      var notif = result.data;
+      if (notif) {
+        return self.registration.showNotification('Le\'Fam Central', {
+          body: notif.Message,
+          icon: '/icon-192.png',
+          badge: '/icon-192.png',
+          data: { page: notif.Page || 'dashboard' }
+        });
+      } else {
+        return self.registration.showNotification('Le\'Fam Central', {
+          body: 'You have new activity!',
+          icon: '/icon-192.png',
+          badge: '/icon-192.png',
+          data: { page: 'dashboard' }
+        });
       }
-      if (clients.openWindow) {
-        return clients.openWindow('./index.html');
-      }
+    })
+    .catch(function() {
+      return self.registration.showNotification('Le\'Fam Central', {
+        body: 'You have new activity!',
+        icon: '/icon-192.png',
+        badge: '/icon-192.png',
+        data: { page: 'dashboard' }
+      });
     })
   );
 });
